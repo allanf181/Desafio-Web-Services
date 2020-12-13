c21b54a42a5d59dbca9b8e2657934e2e399eefc8
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2020-12-12 21:13:29 -0300

    Register Screen

--- app/src/main/java/com/example/desafiowebservices/LoginActivity.kt
+++ app/src/main/java/com/example/desafiowebservices/LoginActivity.kt
@@ -3,10 +3,16 @@ package com.example.desafiowebservices
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        tv_register.setOnClickListener{
+            val i = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(i)
+        }
     }
 }
---
+++ app/src/main/java/com/example/desafiowebservices/RegisterActivity.kt
@@ -0,0 +1,30 @@
+package com.example.desafiowebservices
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setContentView(R.layout.activity_register)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+}
